de89e399901462096e7a1e5dccbdafa5c49e5478
Shaochang Liu <sliu27@wm.edu>
2026-05-11 05:19:33 +0000

    modify to support GH200

--- pacpu/build.sh
+++ pacpu/build.sh
@@ -1,6 +1,6 @@
 Torch_DIR=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)')/Torch
 CUDA_HOST_COMPILER_PATH=$(which g++-11)
-CXX_COMPILER_PATH=$(which g++-13)
+CXX_COMPILER_PATH=$(which g++-11)
 
 mkdir -p build
 cmake -B build -S . -DTorch_DIR=$Torch_DIR -DModel=$1 -DTP=$2 -DCMAKE_CUDA_HOST_COMPILER=${CUDA_HOST_COMPILER_PATH} -DCMAKE_CXX_COMPILER=${CXX_COMPILER_PATH}
